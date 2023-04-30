1e02b79c0dce476f4ba39fc31b712214ec986aac
Duc Trung <trung.lyduc18@gmail.com>
2023-05-01 00:49:00 +0700

    REWORK

--- Week_1/week1_1_1/src/ThreeSort.java
+++ Week_1/week1_1_1/src/ThreeSort.java
@@ -3,39 +3,22 @@ import java.util.Scanner;
 
 public class ThreeSort {
 
-    public static void mySort(int input_length, Scanner scan) {
-        /* Input */
-        if (input_length <= 0) {
-            return;
-        }
-        int[] array = new int[input_length];
-        System.out.println("Enter " + input_length + " integers.");
-        for (int i = 0; i < input_length; i++) {
-            System.out.print("Enter number with index " + (i + 1) + ": ");
-            array[i] = scan.nextInt();
-        }
+    public static void main(String[] args) {
+        int[] array = new int[3];
+        Scanner scan = new Scanner(System.in);
+        array[0] = scan.nextInt();
+        array[1] = scan.nextInt();
+        array[2] = scan.nextInt();
 
-        /* Sort */
         Arrays.sort(array);
 
-        /* Output */
-        System.out.print("Array: ");
         for (int i = 0; i < array.length; i++) {
             if (i != array.length - 1) {
-                System.out.print(array[i] + ", ");
+                System.out.print(array[i] + " ");
             } else {
                 System.out.println(array[i]);
             }
         }
     }
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter length of array number: ");
-        int array_length = scan.nextInt();
-        if (array_length <= 0) {
-            return;
-        }
-        mySort(array_length, scan);
-    }
 }

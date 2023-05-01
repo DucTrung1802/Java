d55076014cdebc7d3b7193973e3f1f82915fe84e
Duc Trung <trung.lyduc18@gmail.com>
2023-05-01 15:59:11 +0700

    COMPLETE: week1_1_3
    http://mim.hus.vnu.edu.vn/mimgo/mod/vpl/view.php?id=2762

---
+++ Week_1/week1_1_3/src/Distance.java
@@ -0,0 +1,13 @@
+public class Distance {
+
+    public static void main(String[] args) {
+        double x1 = Double.parseDouble(args[0]);
+        double y1 = Double.parseDouble(args[1]);
+        double x2 = Double.parseDouble(args[2]);
+        double y2 = Double.parseDouble(args[3]);
+        double distance;
+        distance = Math.sqrt((Math.pow((x1 - x2), 2)) + (Math.pow((y1 - y2), 2)));
+        distance = Math.round(distance * 100.0) / 100.0;
+        System.out.println(distance);
+    }
+}

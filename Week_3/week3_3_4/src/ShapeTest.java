6e93d107582938201d4888ac387178d9f75f7b24
Duc Trung <trung.lyduc18@gmail.com>
2023-05-20 01:27:39 +0700

    COMPLETE: week3_3_4 (untested)
    http://mim.hus.vnu.edu.vn/mimgo/mod/vpl/view.php?id=2839&userid=4610

--- Week_3/week3_3_4/src/ShapeTest.java
+++ Week_3/week3_3_4/src/ShapeTest.java
@@ -5,6 +5,20 @@ import java.util.Scanner;
 public class ShapeTest {
     private static Scanner scan = new Scanner(System.in);
 
+
+    public static void sort(ArrayList<Shape> shapes) {
+        int n = shapes.size();
+
+        for (int i = 0; i < n - 1; i++)
+            for (int j = i + 1; j < n; j++) {
+                if ((shapes.get(i)).compareTo(shapes.get(j)) > 0) {
+                    Shape temp = shapes.get(i);
+                    shapes.set(i, shapes.get(j));
+                    shapes.set(j, temp);
+                }
+            }
+    }
+
     public static void main(String[] args) {
 //        ArrayList<Shape> shapes = new ArrayList<Shape>();
 //
@@ -27,24 +41,51 @@ public class ShapeTest {
 //        }
 
         ShapeTest shape_test = new ShapeTest();
-        shape_test.readData();
+        ArrayList<Shape> shapes = shape_test.readData();
+        System.out.println(getMaxCircle(shapes).getArea());
+        System.out.println(getMaxSquare(shapes).getArea());
+        System.out.println(getMaxRectangle(shapes).getArea());
 
         scan.close();
 
     }
 
-    public static void sort(ArrayList<Shape> shapes) {
-        int n = shapes.size();
-
-        for (int i = 0; i < n - 1; i++)
-            for (int j = i + 1; j < n; j++) {
-                if ((shapes.get(i)).compareTo(shapes.get(j)) > 0) {
-                    Shape temp = shapes.get(i);
-                    shapes.set(i, shapes.get(j));
-                    shapes.set(j, temp);
-                }
+    public static Circle getMaxCircle(ArrayList<Shape> shapes) {
+        double max_area = 0.0d;
+        Circle max_circle = null;
+        for (Shape shape : shapes) {
+            if (shape instanceof Circle && shape.getArea() > max_area) {
+                max_area = shape.getArea();
+                max_circle = (Circle) shape;
             }
+        }
+        return max_circle;
     }
+
+    public static Square getMaxSquare(ArrayList<Shape> shapes) {
+        double max_area = 0.0d;
+        Square max_square = null;
+        for (Shape shape : shapes) {
+            if (shape instanceof Square && shape.getArea() > max_area) {
+                max_area = shape.getArea();
+                max_square = (Square) shape;
+            }
+        }
+        return max_square;
+    }
+
+    public static Rectangle getMaxRectangle(ArrayList<Shape> shapes) {
+        double max_area = 0.0d;
+        Rectangle max_rectangle = null;
+        for (Shape shape : shapes) {
+            if (shape instanceof Rectangle && shape.getArea() > max_area) {
+                max_area = shape.getArea();
+                max_rectangle = (Rectangle) shape;
+            }
+        }
+        return max_rectangle;
+    }
+
 
     public ArrayList<Shape> readData() {
         int n = Integer.parseInt(scan.nextLine());
@@ -54,9 +95,23 @@ public class ShapeTest {
         ArrayList<Shape> shape_array_list = new ArrayList<Shape>();
         for (int i = 0; i < n; i++) {
             char shape_type = scan.next().charAt(0);
-            int number = scan.nextInt();
-            System.out.println(shape_type);
-            System.out.println(number);
+            if (shape_type == 'c') {
+                double radius = scan.nextDouble();
+                Circle circle = new Circle(radius);
+                shape_array_list.add(circle);
+            } else if (shape_type == 's') {
+                double side = scan.nextDouble();
+                Square square = new Square(side);
+                shape_array_list.add(square);
+            } else if (shape_type == 'r') {
+                double length = scan.nextDouble();
+                double width = scan.nextDouble();
+                Rectangle rectangle = new Rectangle(length, width);
+                shape_array_list.add(rectangle);
+            } else {
+                throw new IllegalArgumentException("Shape type is not supported. Supported shape type: c, s, r");
+            }
+
         }
         return shape_array_list;
     }

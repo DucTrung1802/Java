f8e4397a00aaedbf8b2270f77270aeef758556ef
Duc Trung <trung.lyduc18@gmail.com>
2023-05-29 20:33:26 +0700

    CHECKPOINT exam 2

---
+++ Exam/exam2/src/AthleteManager.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AthleteManager {
+
+    public static ArrayList<Athlete> readFile(String filename) {
+        Scanner scan = null;
+        ArrayList<Athlete> arrayAthlete = null;
+        try {
+            scan = new Scanner(new File(filename));
+            arrayAthlete = new ArrayList<>();
+
+            int numAthlete = Integer.parseInt(scan.nextLine());
+            while(scan.hasNext()) {
+                String name = scan.nextLine();
+                int year = Integer.parseInt(scan.nextLine());
+                String sport = scan.nextLine();
+                double weight = Double.parseDouble(scan.nextLine());
+                double height = Double.parseDouble(scan.nextLine());
+                Athlete a = new Athlete(name, year, sport, weight, height);
+                arrayAthlete.add(a);
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return arrayAthlete;
+    }
+
+    public static void printAthletes(ArrayList<Athlete> arrayAthlete) {
+        for (int i = 0; i < arrayAthlete.size(); i++)
+            System.out.println(arrayAthlete.get(i).toString());
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Athlete> arrayAthlete = readFile("src/athletes.txt");
+        printAthletes(arrayAthlete);
+    }
+}
